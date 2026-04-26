cd04704c3a4144df00d503b18724683201d3b5f1
Kimi <kimi@local>
2026-04-26 10:17:26 +0700

    rotate admin token

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 const PORT = process.env.PORT || 3000;
-const ADMIN_PASSWORD = 'JEsusCHrist';
+const ADMIN_PASSWORD = '2TQmwZePcT5k-PBcpY8k4YwPEG3dtNzP';
 
 function now() { return Math.floor(Date.now() / 1000); }
 
